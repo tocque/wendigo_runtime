a171fd0851c36bb058fb08194542e3cb759ca9fa
any <tangjiacheng.h5route@bytedance.com>
2022-02-23 18:27:33 +0800

    feat: image component

---
+++ src/view/components/Image.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { core } from '@/core/core';
+
+const { src } = defineProps<{ src: string }>();
+</script>
+<template>
+    <img :src="core.material.images.images[src].src"/>
+</template>
+<style>
+</style>
