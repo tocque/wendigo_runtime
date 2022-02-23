1adfaa7f38e5b47a41e4549a93add5f49a47078d
any <tangjiacheng.h5route@bytedance.com>
2022-02-23 18:20:32 +0800

    change view size

--- src/constant.ts
+++ src/constant.ts
@@ -1,8 +1,8 @@
 export const TOWER_ID = "wdg3";
 export const VERSION = "0.1";
 
-export const VIEW_WIDTH = 23 * 32;
-export const VIEW_HEIGHT = 13 * 32;
+export const VIEW_WIDTH = 750;
+export const VIEW_HEIGHT = 422;
 
 export const CHUNK_MAP = {
     se: 2,
